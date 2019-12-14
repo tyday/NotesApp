2ac2730f07383879f676212f9835a42430e84c3a
Ty Day <tyrday@gmail.com>
2019-12-13 19:58:09 -0500

    Add project files.

---
+++ NotesApp/Model/Note.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Model
+{
+    class Note
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set 
+			{ 
+				id = value;
+				OnPropertyChanged("Id");
+			}
+		}
+
+		private int notebookId;
+
+		public int NotebookId
+		{
+			get { return notebookId; }
+			set 
+			{ 
+				notebookId = value;
+				OnPropertyChanged("NotebookId");
+			}
+		}
+
+		private string title;
+
+		public string Title
+		{
+			get { return title; }
+			set 
+			{ 
+				title = value;
+				OnPropertyChanged("Title");
+			}
+		}
+
+		private DateTime createdTime;
+
+		public DateTime CreatedTime
+		{
+			get { return createdTime; }
+			set 
+			{ 
+				createdTime = value;
+				OnPropertyChanged("CreatedTime");
+			}
+		}
+
+		private DateTime updatedTime;
+
+		public DateTime UpdatedTime
+		{
+			get { return updatedTime; }
+			set 
+			{ 
+				updatedTime = value;
+				OnPropertyChanged("UpdatedTime");
+			}
+		}
+
+		private string fileLocation;
+
+		public string FileLocation
+		{
+			get { return fileLocation; }
+			set 
+			{ 
+				fileLocation = value;
+				OnPropertyChanged("FileLocation");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+	}
+}
---
+++ NotesApp/Model/User.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Model
+{
+    class User
+    {
+
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; OnPropertyChanged("Id"); }
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; OnPropertyChanged("Name"); }
+		}
+
+		private string lastname;
+
+		public string Lastname
+		{
+			get { return lastname; }
+			set { lastname = value; OnPropertyChanged("Lastname"); }
+		}
+
+		private string username;
+
+		public string Username
+		{
+			get { return username; }
+			set { username = value; OnPropertyChanged("Username"); }
+		}
+
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; OnPropertyChanged("Email"); }
+		}
+
+		private string password;
+
+		public string Password
+		{
+			get { return password; }
+			set { password = value; OnPropertyChanged("Password"); }
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
